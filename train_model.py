cafad0d1052c49054f6c4f35be357cb069469d23
Raunak Adak <raunak249@gmail.com>
2019-02-21 11:15:27 +0530

    Extended the train_model file

--- train_model.py
+++ train_model.py
@@ -9,5 +9,24 @@ from sklearn.svm import SVC
 ap = argparse.ArgumentParser()
 ap.add_argument('-e','--embeddings',required=True,help='Path to the serialized embeddings')
 ap.add_argument('-r','--recognizer',required=True,help='Path to the trained model')
-ap.add_argument('-l','le',required=True,help='Path to the output label encoder')
+ap.add_argument('-l','--le',required=True,help='Path to the output label encoder')
 args = vars(ap.parse_args())
+
+print('[INFO] loading the embeddings..')
+data = pickle.loads(open(args['embeddings'],'rb').read())
+
+#initializing the label encoder
+le = LabelEncoder()
+labels = le.fit_transform(data['names'])
+
+print('[INFO] initializing the model')
+recognizer = SVC(C=1.0,kernel = 'linear',probability=True)
+recognizer.fit(data['embeddings'],labels)
+
+f = open(args['recognizer'],'wb')
+f.write(pickle.dumps(recognizer))
+f.close()
+
+f = open(args['le'],'wb')
+f.write(pickle.dumps(labels))
+f.close()
